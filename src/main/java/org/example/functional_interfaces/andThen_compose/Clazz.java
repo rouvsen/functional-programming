f2a5f9b1f3d54e9cc0e0a1eff1c2d12139c68a1d
rovshan <rouvsen@gmail.com>
2023-04-29 01:36:32 +0400

    andThen && compose methods - added

---
+++ src/main/java/org/example/functional_interfaces/andThen_compose/Clazz.java
@@ -0,0 +1,16 @@
+package org.example.functional_interfaces.andThen_compose;
+
+public class Clazz {//f.e: with Function
+    public static void main(String[] args) {
+
+        // examples about andThen(ilk sol, sonra sag)
+        // and compose(ilk sag, sonra sol)
+
+        //twice.andThen(square) - ilk twice-ini sonra square-ini
+        //twice.compose(square) - ilk square-ini sonra twice-ini
+
+        //multiple antThen or compose
+        //andThen --> moving
+        //compose <-- moving
+    }
+}
