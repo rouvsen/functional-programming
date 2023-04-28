476d8386b86693556df90f79ae81dca64e635920
rovshan <rouvsen@gmail.com>
2023-04-29 01:35:48 +0400

    Using @FunctionalInterface implementation of @FunctionalInterface with Lambda Exp.

--- src/main/java/org/example/lambda/Clazz.java
+++ src/main/java/org/example/lambda/Clazz.java
@@ -2,15 +2,19 @@ package org.example.lambda;
 
 public class Clazz {
     public static void main(String[] args) {
+
         System.out.println("interface1");
         //interface1
         Interface1 interface1 = () -> 5;
         int value = interface1.f();
+
         System.out.println(value + "\n"+"interface2");
         //interface2
         Interface2 interface2 = i -> i;
         value = interface2.f(6);
+
         System.out.println(value + "\n"+"interface3");
+        //interface3
         Interface3 interface3 = () -> System.out.println("Hi");
         interface3.f();
     }
