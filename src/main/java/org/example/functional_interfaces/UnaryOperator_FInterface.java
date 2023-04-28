f1c2b67fc2397bf9cc46c0c17b27430cdc4cd1f7
rovshan <rouvsen@gmail.com>
2023-04-29 01:37:49 +0400

    UnaryOperator - added

---
+++ src/main/java/org/example/functional_interfaces/UnaryOperator_FInterface.java
@@ -0,0 +1,12 @@
+package org.example.functional_interfaces;
+
+public class UnaryOperator_FInterface {
+    // <T> type-inda alir, ele <T> type-inda da return edir
+    // Integer alir, Integer return edir,
+    // f.e: Integer alir square-ini tapir, integer olaraq da return edir
+
+    //Function-i extends edir, Function <T> goturur, <R> return edir
+    //Function<T><R>
+
+    //Bu ise Function-i, Function<T><T> olaraq istifade edir
+}
