35f26befba39e7022668a9e3ee2ae623e66139e4
rovshan <rouvsen@gmail.com>
2023-04-29 01:36:11 +0400

    BinaryOperator added

---
+++ src/main/java/org/example/functional_interfaces/BinaryOperator_FInterface.java
@@ -0,0 +1,15 @@
+package org.example.functional_interfaces;
+
+public class BinaryOperator_FInterface {
+    // <T><T> 2 dene eyni type-da argument alir,
+    // 1 dene <T> eyni type return edir
+    // f.e: 2 dene double alir, double-lari toplayir return edir (double)
+
+    //BinaryOperator - extends edir BiFunction-i
+    //BinaryOperator 1 dene <T> goturur,
+    //ancaq ki oz icerisinde BiFunction<T><T><T> istifade edir
+
+    // BinaryOperator<Double> = BiFunction<Double><Double><Double>
+    // Eyni tipde - 2 deyer goturur, 1 deyer return edir..
+
+}
